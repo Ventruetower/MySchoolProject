9a1ddf1f4e46a6cc633a67efe2218150d100a537
Ventruetower <43596703+Ventruetower@users.noreply.github.com>
2018-09-26 16:29:30 +0900

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,18 @@
+import telebot
+
+from boto.s3.connection import S3Connection
+token = S3Connection(os.environ['TOKEN'])
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): 
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+     bot.polling(none_stop=True)
+        
+        
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
