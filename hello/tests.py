59a3b6c721c661446ee6813cd5884d46b5a72be0
Ventruetower <43596703+Ventruetower@users.noreply.github.com>
2018-09-26 16:21:51 +0900

    Update tests.py

--- hello/tests.py
+++ hello/tests.py
@@ -1,3 +1,16 @@
+import telebot
+
+from boto.s3.connection import S3Connection
+token = S3Connection(os.environ['TOKEN'])
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): 
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+     bot.polling(none_stop=True)
+
 from django.contrib.auth.models import AnonymousUser, User
 from django.test import TestCase, RequestFactory
 
